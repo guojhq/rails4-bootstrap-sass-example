a848627869a5cf374071fea2796a704668b7b65a
Kevin Francis <kfrancis@gmail.com>
2014-11-04 17:07:16 +0800

    Added Sign up for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :users
+
+  get 'signup' => 'users#new'
+
   root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
