b49975a3d44bda7da1a43e2ffcf7fcbcc92543f0
Kevin Francis <kfrancis@gmail.com>
2014-11-06 22:58:33 +0800

    Added new layout, and profile model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106135335) do
+ActiveRecord::Schema.define(version: 20141106144716) do
+
+  create_table "profiles", force: true do |t|
+    t.string   "location"
+    t.date     "birthdate"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
